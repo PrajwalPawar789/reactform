cdd4cbb22255a76931ddb80be27407df5a1c6d30
PrajwalPawar789 <prajwalrpawar2001@gmail.com>
2023-12-27 05:26:47 +0530

    styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
-import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // const [firstname, setFirstname] = useState("");
-  // const [lastname, setLastname] = useState("");
-
-  // console.log('firstname', firstname);
-  // console.log('lastname', lastname);
-
-  // function changelastNameHandler(event) {
-  //   // console.log(event.target.value);
-  //   setLastname(event.target.value);
-  // }
-
-  // function changefirstNameHandler(event){
-  //   // console.log(event.target.value);
-  //   setFirstname(event.target.value);
-  // }
-
   const [formData, setFormData] = useState({
     firstname: "",
     lastname: "",
@@ -27,7 +10,6 @@ function App() {
     mode: "",
     favcar: "",
   });
-  // console.log(formData);
 
   function changeHandler(event) {
     setFormData((prevFormData) => {
@@ -35,108 +17,132 @@ function App() {
       return {
         ...prevFormData,
         [name]: type === "checkbox" ? checked : value,
-        // [event.target.name]: event.target.value
-      }; 
+      };
     });
   }
 
   function submitHandler(event) {
     event.preventDefault();
-    //printing
     console.log("Printing all form data...");
     console.log(formData);
   }
 
   return (
-    <div className="App">
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          placeholder="first name"
-          onChange={changeHandler}
-          name="firstname"
-          value={formData.firstname}
-        ></input>
-        <br></br>
-        <br></br>
-        <input
-          type="text"
-          placeholder="last name"
-          onChange={changeHandler}
-          name="lastname"
-          value={formData.lastname}
-        ></input>
-        <br></br>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={changeHandler}
-          name="email"
-          value={formData.email}
-        ></input>
-        <br></br>
-        <br></br>
-        <textarea
-          placeholder="enter your comment"
-          onChange={changeHandler}
-          name="comment"
-          value={formData.comment}
-        ></textarea>
-        <br />
-        <br />
-        <input
-          type="checkbox"
-          onChange={changeHandler}
-          name="isVisible"
-          id="isVisible"
-          checked={formData.isVisible}
-        />
-        <label htmlFor="isVisible">Am I visible ?</label>
-        <br />
-        <br />
-        <br />
-
-        <fieldset>
-          <legend>Mode:</legend>
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <form onSubmit={submitHandler} className="bg-white p-8 rounded-md shadow-lg">
+        <h2 className="text-2xl font-bold mb-6 text-gray-800">Feedback Form</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="mb-4">
+            <label htmlFor="firstname" className="block text-sm font-medium text-gray-600">
+              First Name
+            </label>
+            <input
+              type="text"
+              placeholder="First Name"
+              onChange={changeHandler}
+              name="firstname"
+              value={formData.firstname}
+              className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastname" className="block text-sm font-medium text-gray-600">
+              Last Name
+            </label>
+            <input
+              type="text"
+              placeholder="Last Name"
+              onChange={changeHandler}
+              name="lastname"
+              value={formData.lastname}
+              className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
           <input
-            type="radio"
+            type="email"
+            placeholder="Email"
             onChange={changeHandler}
-            name="mode"
-            value="online-mode"
-            id="online-mode"
-            checked={formData.mode === "online-mode"}
+            name="email"
+            value={formData.email}
+            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
           />
-          <label htmlFor="online-mode">Online Mode</label>
-
+        </div>
+        <div className="mb-4">
+          <label htmlFor="comment" className="block text-sm font-medium text-gray-600">
+            Comment
+          </label>
+          <textarea
+            placeholder="Enter your comment"
+            onChange={changeHandler}
+            name="comment"
+            value={formData.comment}
+            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          ></textarea>
+        </div>
+        <div className="mb-4">
           <input
-            type="radio"
+            type="checkbox"
             onChange={changeHandler}
-            name="mode"
-            value="Offline-mode"
-            id="Offline-mode"
-            chekced={formData.mode === "Offline-mode"}
+            name="isVisible"
+            id="isVisible"
+            checked={formData.isVisible}
           />
-          <label htmlFor="Offline-mode">Offline Mode</label>
-        </fieldset>
-        <label htmlFor="favcar">Tell me your favcar</label>
-        <select
-          onChange={changeHandler}
-          name="favcar"
-          value={formData.favcar}
-          id="favcar"
-        >
-          
-          <option value="scarpio">Scarpio</option>
-          <option value="Thar">Thar</option>
-          <option value="Defender">Defender</option>
-          <option value="Lambo">Lambo</option>
-          <option value="Ferari">Ferari</option>
-          <option value="Bmw">Bmw</option>
-        </select>
-        <button>
-          submit
+          <label htmlFor="isVisible" className="ml-2 text-sm font-medium text-gray-600">
+            Am I visible?
+          </label>
+        </div>
+        <div className="mb-4">
+          <fieldset>
+            <legend className="text-sm font-medium text-gray-600">Mode:</legend>
+            <label className="block text-sm">
+              <input
+                type="radio"
+                onChange={changeHandler}
+                name="mode"
+                value="online-mode"
+                checked={formData.mode === "online-mode"}
+              />
+              <span className="ml-2">Online Mode</span>
+            </label>
+            <label className="block text-sm">
+              <input
+                type="radio"
+                onChange={changeHandler}
+                name="mode"
+                value="offline-mode"
+                checked={formData.mode === "offline-mode"}
+              />
+              <span className="ml-2">Offline Mode</span>
+            </label>
+          </fieldset>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="favcar" className="block text-sm font-medium text-gray-600">
+            Favorite Car:
+          </label>
+          <select
+            onChange={changeHandler}
+            name="favcar"
+            value={formData.favcar}
+            id="favcar"
+            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          >
+            <option value="scarpio">Scarpio</option>
+            <option value="thar">Thar</option>
+            <option value="defender">Defender</option>
+            <option value="lambo">Lambo</option>
+            <option value="ferari">Ferari</option>
+            <option value="bmw">Bmw</option>
+          </select>
+        </div>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">
+          Submit
         </button>
-
       </form>
     </div>
   );
